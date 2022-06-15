e9fef56bc3b9d507a15d894734e7969c47624564
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-06-15 23:25:20 +0200

    post, put, delete methods are completed

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,53 @@
-const express = require('express')
+const express = require("express");
 const app = express();
- 
+const fs = require("fs");
+const { url } = require("inspector");
 
 // YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.post("/blogs", (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res.status(400).send("Please include a title and content");
+  }
+  fs.writeFileSync(title, content);
+  res.status(201).send(`${title} is added`);
+});
+
+app.put("/posts/:title", (req, res) => {
+  const postTitle = req.params.title;
+  if (!fs.existsSync(postTitle)) {
+    return res
+      .status(400)
+      .send(`No blog post is found with ${postTitle} title`);
+  }
+
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).send("Please include a title and content");
+  }
+  fs.unlinkSync(postTitle);
+  fs.writeFileSync(title, content);
+  res.status(200).send("Blog post is updated");
+});
+
+app.delete("/blogs/:title", (req, res) => {
+  const postTitle = req.params.title;
+  console.log(postTitle);
+  if (!fs.existsSync(postTitle)) {
+    return res
+      .status(400)
+      .send(`No blog post is found with ${postTitle} title`);
+  }
+  fs.unlinkSync(postTitle);
+  res.status(200).send("Blog post is deleted");
+});
+
+app.listen(3000);
